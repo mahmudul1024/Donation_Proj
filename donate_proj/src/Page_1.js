627ffb166c9ead7c6efd2e73a859797ce109b5d1
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-07-06 17:07:32 +0200

    1st page complete

--- donate_proj/src/Page_1.js
+++ donate_proj/src/Page_1.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Page_1 = () => {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <>
+    <section>
+      <div className='background_1'> </div>
+      <div className='text_box_1'>
+      <h1>Let'sHelp Those Who Are More In Need </h1>
+        <p> Lorem ipsum dolor sit amet, usu cu esse possit referrentur, at eam falli deterruisset. No duo populo animal noluisse, enim</p>
+        <button className='donate_now_btn'>Donate Now</button>
+      </div>
+      
+
+
+
+
+
+    </section>
+
+    </>
   )
 }
 
