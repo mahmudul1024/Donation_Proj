49f7df19fc79a1a118f8b9ebd305b47437148938
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-07-06 23:18:22 +0200

    third

--- donate_proj/src/Page_1.js
+++ donate_proj/src/Page_1.js
@@ -16,6 +16,43 @@ const Page_1 = () => {
 
 
 
+    </section >
+    <section >
+    <div className='textbox_2'>
+        <h1>Donate Today</h1>
+        <p>Lorem ipsum dolor sit amet, usu cu esse possit referrentur, at eam falli deterruisset. No duo populo animal noluisse, enim</p>
+
+        
+
+
+        <div className='rectangle_container'>
+            <div className='rec_text'>
+                <p>1000tk</p>
+            </div>
+            <div className='rec_text'>
+                <p>2000tk</p>
+            </div>
+            <div className='rec_text'>
+                <p>3000tk</p>
+            </div>
+            <div className='rec_text'>
+                <p>500tk</p>
+            </div>
+            <div className='rec_text'>
+                <p>1000tk</p>
+            </div>
+        
+
+        </div>
+
+        <form>
+
+<input className='text' type={"text"} placeholder="Other Amount (Taka)"></input>
+    </form>
+    </div>
+
+    
+    
     </section>
 
     </>
