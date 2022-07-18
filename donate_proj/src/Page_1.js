74fa67b5a7cbcef42d4b799367cd3f4100745fff
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-07-18 02:19:36 +0200

    m

--- donate_proj/src/Page_1.js
+++ donate_proj/src/Page_1.js
@@ -86,7 +86,7 @@ const Page_1 = () => {
             <div className='donate_rectangle'>
                 <div className='image_icon'>
                 <div className='shift_left_image'>
-                <img src='/icons/shelter.png' ></img>
+                <img src='icons/shelter.png' ></img>
                 </div>
                 <div className='shift_text'></div>
                 </div>
